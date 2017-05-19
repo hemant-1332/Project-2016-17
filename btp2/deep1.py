4aa483d06f86324b5cfefe9068cb31897d7190b8
hemant-1332 <striker.hemant@gmail.com>
2017-05-19 17:01:42 +0530

    Add files via upload

---
+++ btp2/deep1.py
@@ -0,0 +1,6 @@
+from nolearn.dbn import DBN
+from sklearn.metrics import classification_report
+from sklearn.cross_validation import train_test_split
+from sklearn import datasets
+import numpy as np
+import cv2
